6a141b134564a49a6ef071683c2e59cc48b7096d
tanim-k <tanimkhan877@gmail.com>
2022-02-15 12:18:05 +0600

    substr and substring

--- string-slice.js
+++ string-slice.js
@@ -5,6 +5,17 @@ const allLetters = anthem.split('');
 const words = anthem.split(' ');
 // to split with specific letter
 const withoutA = anthem.split('a');
+// how to find a apecific slice :
+const smallSlice = anthem.slice(5,10);
+// to find a part as ur wish
+const wishPart = anthem.substr(5, 8);
+// another property
+const itsLikeSlice = anthem.substring(5, 8);
+
+
 console.log(allLetters);
 console.log(words);
 console.log(withoutA);
+console.log(smallSlice);
+console.log('wishpart:', wishPart);
+console.log('sub-string:', itsLikeSlice);
