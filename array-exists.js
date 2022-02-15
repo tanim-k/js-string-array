4b929ece3a5ee8f9fde102e0cebfd3f359cc5cd0
tanim-k <tanimkhan877@gmail.com>
2022-02-15 17:41:26 +0600

    array exist and error handeling

--- array-exists.js
+++ array-exists.js
@@ -18,3 +18,15 @@ const myBigBuddy = megaFriend(friends);
 const myBigBuddyError = megaFriend(12434);
 console.log(myBigBuddy);
 console.log(myBigBuddyError);
+
+// to check a name in an array 
+if(friends.indexOf('jafur') != -1) {
+    console.log('jafur exists');
+}
+// to check a name by using includes
+if(friends.includes('shamol')){
+    console.log('shamol is included');
+}
+if(friends.includes('shamol')== true){
+    console.log('shamol is there.its true')
+}
