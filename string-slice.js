3be5393f12a1cf6585e38b1fc122fd152936f4a1
tanim-k <tanimkhan877@gmail.com>
2022-02-15 12:51:01 +0600

    concat and addstring

--- string-slice.js
+++ string-slice.js
@@ -11,11 +11,16 @@ const smallSlice = anthem.slice(5,10);
 const wishPart = anthem.substr(5, 8);
 // another property
 const itsLikeSlice = anthem.substring(5, 8);
-
-
+// concat to add two strings
+const first = 'marry';
+const second = ' me';
+const fullString = first + second;
+const fullByConcat = first.concat(second).concat(' pera');
 console.log(allLetters);
 console.log(words);
 console.log(withoutA);
 console.log(smallSlice);
 console.log('wishpart:', wishPart);
 console.log('sub-string:', itsLikeSlice);
+console.log('add>string:', fullString);
+console.log('add>string:', fullByConcat);
